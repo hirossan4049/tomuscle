7c29f1766784e383333ed22017be85cd46af04fd
hirossan4049 <haruto405329@gmail.com>
2025-09-06 20:55:53 +0900

    Initial Commit

---
+++ tomuscle/tomuscleApp.swift
@@ -0,0 +1,34 @@
+//
+//  tomuscleApp.swift
+//  tomuscle
+//
+//  Created by a on 9/6/25.
+//
+
+import SwiftUI
+
+@main
+struct tomuscleApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+        .windowStyle(.volumetric)
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+    }
+}
